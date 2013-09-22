8e7c0d4a74b1704b1891e4078e33f117c073b75b
Ral <contato@raloliver.com>
2013-09-22 09:42:20 -0300

    Number
    Pense em um número...

---
+++ number.php
@@ -0,0 +1,6 @@
+<?php 
+		$x = 12;
+		$value = 16; // o valor vai ser sempre o que você somou divivido por dois ($result = $value /2)
+		$result = ((($x * 2) + $value) / 2) - $x;
+		echo $result.'<hr/>';
+?>
